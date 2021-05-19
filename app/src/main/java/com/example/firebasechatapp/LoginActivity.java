e6efad6a871c1d3bb5d6a940c3b44d8bbf3ba2af
TimaOrazkulov <tima.orazkulov@gmail.com>
2021-05-20 02:35:39 +0600

    Login Activity functionality added.

--- app/src/main/java/com/example/firebasechatapp/LoginActivity.java
+++ app/src/main/java/com/example/firebasechatapp/LoginActivity.java
@@ -1,21 +1,44 @@
 package com.example.firebasechatapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
     private TextView register;
+    private EditText editTextEmail, editTextPassword;
+    private Button signIn;
 
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         register = (TextView) findViewById(R.id.login_register_text_view);
         register.setOnClickListener(this);
+        signIn = (Button)findViewById(R.id.login_button);
+        signIn.setOnClickListener(this);
+        editTextEmail = (EditText)findViewById(R.id.email_input);
+        editTextPassword = (EditText) findViewById(R.id.password_input);
+        mAuth = FirebaseAuth.getInstance();
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
     }
 
     @Override
@@ -23,6 +46,60 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         switch (v.getId()){
             case R.id.login_register_text_view:
                 startActivity(new Intent(this, RegisterActivity.class));
+            case R.id.login_button:
+                userLogin();
+                break;
         }
+    }
+
+    private void userLogin() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (email.isEmpty()){
+            editTextEmail.setError("Email is required!");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Please enter a valid email!");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if (password.isEmpty()){
+            editTextPassword.setError("Password is required!");
+            editTextPassword.requestFocus();
+            return;
+        }
+        if (password.length() < 6){
+            editTextPassword.setError("Min password length is 6 characters!");
+            editTextPassword.requestFocus();
+            return;
+        }
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    // redirect to chat
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    if (user.isEmailVerified()){
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        intent.putExtra("userId",user.getUid());
+                        startActivity(intent);
+                    }else{
+                        user.sendEmailVerification();
+                        Toast.makeText(LoginActivity.this, "Check your email for verification!",
+                                Toast.LENGTH_LONG).show();
+                    }
+                    progressBar.setVisibility(View.GONE);
+                }else{
+                    Toast.makeText(LoginActivity.this, "Failed to Login! Please check your credentials!",
+                            Toast.LENGTH_LONG).show();
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 }
