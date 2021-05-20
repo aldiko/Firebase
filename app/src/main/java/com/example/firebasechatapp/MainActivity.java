58f1d818d6d842c0158a8434139d9a6cd291ae63
Your name <orazbek20029@mail.ru>
2021-05-20 06:10:34 +0600

    сделал вывод по нику, но скорее всего немного через костыль, надо почистить код и еще может добавить правки в xml

--- app/src/main/java/com/example/firebasechatapp/MainActivity.java
+++ app/src/main/java/com/example/firebasechatapp/MainActivity.java
@@ -4,10 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.InputFilter;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,11 +17,18 @@ import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,23 +48,81 @@ public class MainActivity extends AppCompatActivity {
     DatabaseReference messagesDatabaseReference;
     ChildEventListener messagesChildEventListener;
 
+    private FirebaseAuth mAuth;
+    DatabaseReference userDatabaseReference;
+    //private List<String> userInfo;
+    private Intent i;
+
+
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        // Check if user is signed in (non-null) and update UI accordingly.
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        if(currentUser != null){
+//            currentUser.reload();
+//        }
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        username="Def User";
+        progressBar = findViewById(R.id.progessBar);
+        sendImageButton = findViewById(R.id.sendPhotoButton);
+        sendMessageButton = findViewById(R.id.sendMessageButton);
+        messageEditText = findViewById(R.id.messageEditText);
+
+
+        
+
+        i = getIntent();
+        String uid = i.getStringExtra("userId");
+        userDatabaseReference=FirebaseDatabase.getInstance().getReference().child("Users");
+        userDatabaseReference.child(uid).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userClass = snapshot.getValue(User.class);
+                username = userClass.fullName;
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+//        ArrayList<String> logAndPass = new ArrayList<>();
+//        logAndPass = i.getExtras().getStringArrayList("userId");
+//        String email = logAndPass.get(0);
+//        String password = logAndPass.get(1);
+//        mAuth = FirebaseAuth.getInstance();
+//        mAuth.signInWithEmailAndPassword(email, password)
+//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        if (task.isSuccessful()) {
+//                            // Sign in success, update UI with the signed-in user's information
+//
+//                            FirebaseUser user = mAuth.getCurrentUser();
+//                            //updateUI(user);
+//                        } else {
+//                            // If sign in fails, display a message to the user.
+//                        }
+//                    }
+//                });
+
+
+
+
+        //getUserInfo();//метод в котором читаем пользователя и достаем данные
 
         //realtime database
         database = FirebaseDatabase.getInstance();
         messagesDatabaseReference = database.getReference().child("messages");
         //
 
-        progressBar = findViewById(R.id.progessBar);
-        sendImageButton = findViewById(R.id.sendPhotoButton);
-        sendMessageButton = findViewById(R.id.sendMessageButton);
-        messageEditText = findViewById(R.id.messageEditText);
+
 
 
         List<MessageModel> messageModels = new ArrayList<>();
@@ -153,5 +220,40 @@ public class MainActivity extends AppCompatActivity {
         };
 
         messagesDatabaseReference.addChildEventListener(messagesChildEventListener);
+
+    }
+
+
+    public void getUserInfo(){
+        userDatabaseReference=FirebaseDatabase.getInstance().getReference().child("Users");
+
+
+        ArrayList<String> logAndPass = new ArrayList<>();
+        logAndPass = i.getExtras().getStringArrayList("userId");
+
+        if (logAndPass.get(0)!=null && logAndPass.get(1)!=null) {
+            mAuth.signInWithEmailAndPassword(logAndPass.get(0), logAndPass.get(1)).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    if (user != null) {
+                        userDatabaseReference.child(user.getUid()).addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                //GenericTypeIndicator<List<String>> gt = new GenericTypeIndicator<List<String>>() {};
+                                //userInfo = snapshot.getValue(gt);
+                                User userClass = snapshot.getValue(User.class);
+                                username = userClass.fullName;
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
+                    }
+                }
+            });
+        }
     }
 }
